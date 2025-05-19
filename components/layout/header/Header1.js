fe89a5d4e408e2497ac68a7a9b9a3707e0abdf09
SWETHACS17 <swetha.cs17@gmail.com>
2025-05-20 00:44:30 +0530

    Added FollowUs bar to Homepage header

--- components/layout/header/Header1.js
+++ components/layout/header/Header1.js
@@ -5,19 +5,53 @@ export default function Header1({ scroll, isMobileMenu, handleMobileMenu }) {
     return (
         <>
             <header>
+                {/* Added Topbar Section */}
+                <div className="tg-header__top">
+                    <div className="container custom-container">
+                        <div className="row align-items-center">
+                            {/* Desktop/Tablet View */}
+                            <div className="col-md-6 d-none d-md-block">
+                                <ul className="tg-header__top-info list-wrap">
+                                    <li><i className="flaticon-email" /> <Link href="mailto:info@gmail.com">info@metatroncubeacademy.com</Link></li>
+                                </ul>
+                            </div>
+                            <div className="col-md-6 d-none d-md-block">
+                                <ul className="tg-header__top-social list-wrap">
+                                    <li>Follow Us On:</li>
+                                    <li><Link href="#"><i className="fab fa-facebook-f" /></Link></li>
+                                    <li><Link href="#"><i className="fab fa-twitter" /></Link></li>
+                                    <li><Link href="#"><i className="fab fa-linkedin-in" /></Link></li>
+                                    <li><Link href="#"><i className="fab fa-youtube" /></Link></li>
+                                </ul>
+                            </div>
+                            
+                            {/* Mobile View */}
+                            <div className="col-12 d-md-none text-center">
+                                <ul className="tg-header__top-social list-wrap" style={{ justifyContent: 'center' }}>
+                                    <li>Follow Us:</li>
+                                    <li><Link href="#"><i className="fab fa-facebook-f" /></Link></li>
+                                    <li><Link href="#"><i className="fab fa-twitter" /></Link></li>
+                                    <li><Link href="#"><i className="fab fa-instagram" /></Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Existing Header Content */}
                 <div id="sticky-header" className={`tg-header__area transparent-header ${scroll ? "sticky-menu" : ""}`}>
                     <div className="container custom-container">
                         <div className="row">
-                            <div className="col-12">
+                            <div className="col-12" style={{ marginTop: '40px'}}>
                                 <div className="mobile-nav-toggler" onClick={handleMobileMenu}><i className="tg-flaticon-menu-1" /></div>
-                                <div className="tgmenu__wrap">
+                                <div className="tgmenu__wrap"  >
                                     <nav className="tgmenu__nav" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
                                         <div className="logo" style={{ flex: '0 0 auto', marginRight: 'auto' }}>
                                             <Link href="/">
                                                 <img src="/assets/img/logo/logo.png" alt="Logo" style={{ width: '250px', maxWidth: 'none' }}/>
                                             </Link>
                                         </div>
-                                        <div className="tgmenu__navbar-wrap tgmenu__main-menu d-none d-xl-flex" style={{ marginLeft: 'auto' }}>
+                                        <div className="tgmenu__navbar-wrap tgmenu__main-menu d-none d-xl-flex" style={{ marginLeft: 'auto'  }}>
                                             <ul className="navigation" style={{ display: 'flex', gap: '30px', marginLeft: 'auto' }}>
                                                 <li><Link href="/">Home</Link></li>
                                                 <li><Link href="/about-us">About Us</Link></li>
@@ -45,9 +79,28 @@ export default function Header1({ scroll, isMobileMenu, handleMobileMenu }) {
                                         <div className="tgmobile__menu-outer">
                                             <MobileMenu />
                                         </div>
+                                        {/* Added Mobile Social Links */}
+                                        <div className="social-links" style={{ 
+                                            padding: '15px 0', 
+                                            textAlign: 'center',
+                                            borderTop: '1px solid rgba(255,255,255,0.1)'
+                                        }}>
+                                            <ul className="list-wrap" style={{ 
+                                                display: 'flex', 
+                                                justifyContent: 'center', 
+                                                gap: '15px',
+                                                margin: 0,
+                                                padding: 0
+                                            }}>
+                                                <li><Link href="#"><i className="fab fa-facebook-f" /></Link></li>
+                                                <li><Link href="#"><i className="fab fa-twitter" /></Link></li>
+                                                <li><Link href="#"><i className="fab fa-instagram" /></Link></li>
+                                                <li><Link href="#"><i className="fab fa-linkedin-in" /></Link></li>
+                                            </ul>
+                                        </div>
                                     </nav>
                                 </div>
-                                <div className="tgmobile__menu-backdrop" onClick={handleMobileMenu} />
+                                <div className="tgmobile__menu-backdrop" onClick={handleMobileMenu}  />
                                 {/* End Mobile Menu */}
                             </div>
                         </div>
